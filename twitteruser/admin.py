ca7b01cf9fd20ffa84b2766bf7f9e9d0829d3708
Enrique Galindo <egalindo@protonmail.com>
2020-02-25 17:58:46 -0500

    fixed typo in twitteruser/admin.py

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import TwitterUser
+from .models import TwitterUser
 
 # Register your models here.
-admin.site.Register(TwitterUser)
+admin.site.register(TwitterUser)
